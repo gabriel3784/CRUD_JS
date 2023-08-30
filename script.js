5f1cb66748eb2a7e04d590a9360dd15966e9e596
Gabriel Silva <gabriel.prog84@gmail.com>
2023-08-30 15:06:53 -0300

    correct file JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector('modal-container')
+const modal = document.querySelector('.modal-container')
 const tbody = document.querySelector('tbody')
 const sNome = document.querySelector('#m-nome')
 const sFuncao = document.querySelector('#m-funcao')
@@ -8,90 +8,89 @@ const btnSalvar = document.querySelector('#btnSalvar')
 let itens
 let id
 
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
 
-function loadItens() {
-    itens = getItensBD()
-    tbody.innerHTML = ''
-    itens.forEach((item, index) => {
-        insertItem(item, index)
-    })
-}
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
 
-loadItens()
-
-function insertItem(item, index) {
-    let tr = document.createElement('tr')
-
-    tr.innerHTML = `
-        <td>${item.nome}</td>
-        <td>${item.funcao}</td>
-        <td>R$ ${item.salario}</td>
-        <td class="acao">
-            <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
-        </td>
-        <td class="acao">
-            <button onclick="deleteItem(${index})"><i class='bx bx-trash' ></i></button>
-        </td>
-    `
-   tbody.appendChild(tr)
+  if (edit) {
+    sNome.value = itens[index].nome
+    sFuncao.value = itens[index].funcao
+    sSalario.value = itens[index].salario
+    id = index
+  } else {
+    sNome.value = ''
+    sFuncao.value = ''
+    sSalario.value = ''
+  }
+  
 }
 
 function editItem(index) {
 
-    openModal(true, index)
+  openModal(true, index)
 }
 
 function deleteItem(index) {
-
-    itens.splice(index, 1)
-    setItensBD()
-    loadItens()
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
 }
 
-function openModal(edit = false, index = 0) {
-    modal.classList.add('active')
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
 
-    modal.onclick = e => {
-        if (e.target.className.indexOf('modal-container') !== -1) {
-            modal.classList.remove('active')
-        }
-    }
-
-    if (edit) {
-        sNome.value = itens[index].nome
-        sFuncao.value = itens[index].funcao
-        sSalario.value = itens[index].salario
-        id = index
-    } else {
-        sNome.value = ''
-        sFuncao.value = ''
-        sSalario.value = ''
-    }
+  tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.funcao}</td>
+    <td>R$ ${item.salario}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
 }
 
 btnSalvar.onclick = e => {
-
-    if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
+  
+  if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
     return
-    }
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 
-    if (id !== undefined) {
-        itens[id].nome = sNome.value
-        itens[id].funcao = sFuncao.value
-        itens[id].salario = sSalario.value
-    } else {
-        itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value})
-    }
+  if (id !== undefined) {
+    itens[id].nome = sNome.value
+    itens[id].funcao = sFuncao.value
+    itens[id].salario = sSalario.value
+  } else {
+    itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value})
+  }
 
-    setItensBD()
+  setItensBD()
 
-    modal.classList.remove('active')
-    loadItens()
-    id = undefined
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
 
 }
 
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
